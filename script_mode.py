492a4c6e77e9843c6dbfd038f9646380a7e7b3c9
Matheus Ribeiro <94936606+punkmic@users.noreply.github.com>
2023-02-07 21:40:54 -0300

    Add files via upload

---
+++ script_mode.py
@@ -0,0 +1,20 @@
+from sagemaker.pytorch import PyTorch
+from sagemaker import get_execution_role
+
+# Define model hyperparameters
+hyperparameters = {"epochs": "2", "batch-size": "64", "test-batch-size": "100", "lr": "0.001"}
+
+# Create a Pytorch estimator
+estimator = Pytorch(
+    entry_point="scripts/pytorch_mnist.py",
+    base_job_name="pytorch_sagemaker",
+    role=get_execution_role(),
+    instance_count=1,
+    instance_type="ml.m5.large",
+    hyperparameters=hyperparameters,
+    framework_version="1.8",
+    py_version="py36"
+)
+
+# Train model
+estimator.fit(wait=True)
